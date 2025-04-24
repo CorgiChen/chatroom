d89d0fdb5672628f65ed439b32f7b3f3a49191d0
CorgiChen <qq882517@gmail.com>
2025-04-24 13:18:08 +0800

    firebase deploy test

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,7 @@
-adasd// src/firebase/config.js
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCDruC3puBO9d_3VVs7uPKXR2rv6gq-_zc",
@@ -15,7 +14,9 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+const db = getFirestore(app);
+const storage = getStorage(app);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+export { auth, googleProvider, db, storage };
